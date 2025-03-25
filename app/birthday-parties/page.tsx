6c12e618231e24c0cca9b428024eb5e3ca22e07a
otitoju <dev_team@plantec.io>
2025-03-25 12:04:45 +0100

    initial commit

--- app/birthday-parties/page.tsx
+++ app/birthday-parties/page.tsx
@@ -11,7 +11,8 @@ import { MainContentSection } from "@/components/about/MainContentSection/MainCo
 import { BenefitsOverviewSection } from "@/components/about/BenefitsOverviewSection";
 // import { ClientTestimonialsSection } from "@/components/about/ClientTestimonialsSection/ClientTestimonialsSection";
 import { ContactUsSection } from "@/components/about/ContactUsSection/ContactUsSection";
-
+import Img from "@/public/assets/Oalh2MojUuk (2).png";
+import { GreetingsSection } from "./partials/Greetings";
 export default function about() {
 
   return (
@@ -21,10 +22,10 @@ export default function about() {
           {/* Main Content */}
           <main className="w-full">
             {/* Offerings Section */}
-            <section
+            {/* <section
               className="w-full relative z-10"
               style={{
-                backgroundImage: "url('/assets/about1.svg')",
+                backgroundImage: "url('/assets/Oalh2MojUuk (2).png')",
                 backgroundSize: "cover",
                 backgroundPosition: "center",
                 backgroundRepeat: "no-repeat",
@@ -32,18 +33,54 @@ export default function about() {
               }}
             >
               <OfferingsSection />
-            </section>
+            </section> */}
 
+            {/* <section className="relative">
+              <div className="h-[500px] bg-cover bg-center" style={{backgroundImage: "url('/assets/Oalh2MojUuk (2).png')"}}>
+                <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+                  <h1 className="text-6xl md:text-8xl font-bold text-center">BIRTHDAY<br/>PARTIES</h1>
+                  <p className="mt-4 text-xl text-center">Our weekend football coaching sessions</p>
+                </div>
+              </div>
+            
+              <div className="relative h-24">
+                <div className="absolute -top-24 w-full overflow-hidden">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none" className="w-full h-24">
+                    <path fill="#EF4444" d="M0,96L80,106.7C160,117,320,139,480,144C640,149,800,139,960,122.7C1120,107,1280,85,1360,74.7L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+                  </svg>
+                </div>
+              </div>
+            </section> */}
+
+            <section className="relative">
+              <div className="h-[680px] w-full relative overflow-hidden">
+
+                <Image src={'https://images.unsplash.com/photo-1606841466847-544b0c481c98?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGJpcnRoZGF5JTIwYm95fGVufDB8fDB8fHww'} alt="Birthday Party Hero" className="absolute inset-0 w-full h-full object-cover" width={1440} height={927}/>
+                  
+                  <div className="absolute inset-0 flex flex-col items-center justify-center text-white z-10">
+                    <h1 className="text-6xl md:text-8xl font-bold text-center">BIRTHDAY<br/>PARTIES</h1>
+                    <p className="mt-4 text-xl text-center">Our weekend football coaching sessions</p>
+                  </div>
+              </div>
+             
+              <div className="relative h-24">
+                <div className="absolute -top-24 w-full overflow-hidden">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none" className="w-full h-24">
+                    <path fill="#FF0000" d="M0,96L80,106.7C160,117,320,139,480,144C640,149,800,139,960,122.7C1120,107,1280,85,1360,74.7L1440,64L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path>
+                  </svg>
+                </div>
+              </div>
+            </section>
             {/* Introduction Section */}
             <section
-              className="w-full relative bg-[#FF0000] mt-[-30px] z-0 py-[50px]"
+              className="w-full relative bg-[#FF0000] mt-[-120px] z-0 py-20"
               style={{
                 backgroundImage: "url('/assets/hero2.svg')",
                 backgroundSize: "cover",
                 backgroundPosition: "center",
               }}
             >
-              <IntroductionSection />
+              <GreetingsSection />
             </section>
 
             {/* Services Overview Section */}
@@ -54,21 +91,20 @@ export default function about() {
             {/* What We Offer Heading */}
             <section className="w-full flex flex-col items-center gap-4 py-16">
               <h2 className="font-desktop-title-headline-2 text-black text-center text-[72px] leading-[72px] tracking-[-1.44px]">
-              MEET THE TEAM 
-              <br />
-              BEHIND THE MAGIC
+              SAY HELLO TO OUR BIZZY BUDDIES
+                <br />
+                BIRTHDAY PARTIES
               </h2>
               <p className="font-desktop-title-subheading-2 text-[#636362] text-center text-lg leading-8">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.  <br />
-              In viverra metus sit amet neque sodales, at sodales ex pretium
+              We do parties for children of any age, run off energy, enjoy games, and provide a full party service
               </p>
             </section>
 
             {/* Image Gallery Section */}
-            <ImageGallerySection />
+            {/* <ImageGallerySection /> */}
 
             {/* Key Features Section */}
-            <KeyFeaturesSection />
+            {/* <KeyFeaturesSection /> */}
 
             {/* Image Display Section */}
             {/* <ImageDisplaySection /> */}
@@ -82,8 +118,8 @@ export default function about() {
             {/* Benefits Overview Section */}
             {/* <BenefitsOverviewSection /> */}
 
-            <section className="w-full p-4">
-              {/* First Row - Two Images */}
+            {/* <section className="w-full p-4">
+           
               <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                 <Image
                   className="w-full h-auto sm:h-[300px] md:h-[415px] object-cover rounded-lg"
@@ -101,7 +137,7 @@ export default function about() {
                 />
               </div>
 
-              {/* Second Row - Two Images in a Flexbox */}
+          
               <div className="flex flex-col sm:flex-row gap-4 mt-4">
                 <Image
                   className="w-full sm:w-1/4 h-auto sm:h-[300px] md:h-[415px] object-cover rounded-lg"
@@ -113,17 +149,12 @@ export default function about() {
                 <Image
                   className="w-full sm:w-3/4 h-auto sm:h-[300px] md:h-[415px] object-cover rounded-lg"
                   alt="Kids in Superhero Masks"
-                  src="/assets/a3.svg"                  width={500}
+                  src="/assets/a3.svg" width={500}
                   height={415}
                 />
               </div>
-            </section>
-
-            {/* Client Testimonials Section */}
-            {/* <ClientTestimonialsSection /> */}
-
-            {/* Contact Us Section */}
-            <ContactUsSection />
+            </section> */}
+            {/* <ContactUsSection /> */}
           </main>
         </div>
       </div>
---
+++ app/birthday-parties/partials/Greetings.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+// import Image from "next/image";
+
+export const GreetingsSection = () => {
+    return (
+        <section className="w-full py-16">
+            <div className="w-full bg-bb-red overflow-hidden border-0">
+                <div className="p-0 relative">
+                    <div className="absolut inset-0 flex items-center justify-center">
+                        <div className="flex flex-col items-center gap-4 text-white max-w-3xl">
+                            <h2 className="font-desktop-title-headline-2 text-[length:var(--desktop-title-headline-2-font-size)] text-center tracking-[var(--desktop-title-headline-2-letter-spacing)] leading-[var(--desktop-title-headline-2-line-height)] text-3xl md:text-6xl">
+                                Say hello to our Bizzy Buddies Birthday Parties
+                            </h2>
+
+                            <p className="font-desktop-title-subheading-2 text-[length:var(--desktop-title-subheading-2-font-size)] text-center tracking-[var(--desktop-title-subheading-2-letter-spacing)] leading-[var(--desktop-title-subheading-2-line-height)] max-w-[300px]">
+                                We don't offer the same old, run of the
+                                mill birthday party service
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
